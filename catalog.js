a4e555f2bf907454672a292271b1bc9aa6c76e90
DenDor <denisdorogov@yandex.ru>
2019-03-19 12:38:10 +0500

    Удалил запятые, убрал лишние переменные, добавил значения по умолчанию, добавил картинку по умолчанию, скорректировал стиль картинок,

--- catalog.js
+++ catalog.js
@@ -1,20 +1,20 @@
 const goods = [
-    {name: 'T-shirt man', price: 50, urlSingle: 'single_page.html', srcImg: 'img/Item1.jpg', altImg: 'Item' },
-    {name: 'Rouse-blose', price: 52, urlSingle: 'single_page.html', srcImg: 'img/Item2.jpg', altImg: 'Item' },
-    {name: 'Blue jacket', price: 84, urlSingle: 'single_page.html', srcImg: 'img/Item3.jpg', altImg: 'Item' },
-    {name: 'Summer dress', price: 61, urlSingle: 'single_page.html', srcImg: 'img/Item4.jpg', altImg: 'Item' },
-    {name: 'Stripy dress', price: 62, urlSingle: 'single_page.html', srcImg: 'img/Item5.jpg', altImg: 'Item' },
-    {name: 'Gray suit', price: 100, urlSingle: 'single_page.html', srcImg: 'img/Item6.jpg', altImg: 'Item' },
-    {name: 'Beige breeches', price: 43, urlSingle: 'single_page.html', srcImg: 'img/Item7.jpg', altImg: 'Item' },
-    {name: 'Blue hoodie', price: 59, urlSingle: 'single_page.html', srcImg: 'img/Item8.jpg', altImg: 'Item' }
+    {name: 'T-shirt man', price: 50, urlSingle: 'single_page.html', srcImg: 'img/Item1.jpg', altImg: 'T-shirt man' },
+    {name: 'Rouse-blose', price: 52, urlSingle: 'single_page.html', srcImg: 'img/Item2.jpg', altImg: 'Rouse-blose' },
+    {name: 'Blue jacket', price: 84, urlSingle: 'single_page.html', srcImg: 'img/Item3.jpg', altImg: 'Blue jacket' },
+    {name: 'Summer dress', price: 61, urlSingle: 'single_page.html', srcImg: 'img/Item4.jpg', altImg: 'Summer dress' },
+    {name: 'Stripy dress', price: 62, urlSingle: 'single_page.html', srcImg: 'img/Item5.jpg', altImg: 'Stripy dress' },
+    {name: 'Gray suit', price: 100, urlSingle: 'single_page.html', srcImg: 'img/Item6.jpg', altImg: 'Gray suit' },
+    {name: 'Beige breeches', price: 43, urlSingle: 'single_page.html', srcImg: 'img/Item7.jpg', altImg: 'Beige breeches' },
+    {name: 'Blue hoodie', price: 59, urlSingle: 'single_page.html', altImg: 'Blue hoodie' }
 ];
 
-const renderItem = ({name, price, urlSingle, srcImg, altImg}) => `<div class="item"><a href= ${urlSingle}><img src= ${srcImg} alt=${altImg}><div class="item-text"><p>${name}</p><h3>$${price}</h3></div></a><a href="#cart" class="add">Add to Card</a></div>`;
+const renderItem = ({name, price, urlSingle, srcImg = 'img/NoPhoto.jpg', altImg = 'good'}) => `<div class="item"><a href= ${urlSingle}><img src= ${srcImg} alt=${altImg}><div class="item-text"><p>${name}</p><h3>$${price}</h3></div></a><a href="#cart" class="add">Add to Card</a></div>`;
 
+const renderList = items => document.querySelector('.items').innerHTML = items.map(renderItem).join('') + `<div class="item" style="height: 0; margin-bottom: 0">`; // При выводе массива, между элементами расставляются запятые. Чтобы избавиться от них, применил метот join.
 
-
-const renderList = items => {
-    const itemsHtmls = items.map(renderItem);
-    document.querySelector('.items').innerHTML = itemsHtmls + `<div class="item" style="height: 0; margin-bottom: 0">`;
-}
 renderList(goods);
+
+
+
+// = {'Good', '0', 'single_page.html', 'img/NoPhoto.jpg', 'Item' }
