327b3706210d4714be1507993649940f28e35537
DenDor <denisdorogov@yandex.ru>
2019-03-21 17:22:33 +0500

    Сделал добавление элементов также на главную страницу. Автоматизировал добавление пустых элементов, для соблюдения порядка элементов на странице.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const CATALOGUE_ROW_LENGTH = 3;
-const CATALOGUE_LENGTH = CATALOGUE_ROW_LENGTH * 4;
+const FEAUTERED_COUNT = 5;
+const FEAUTERED_ROW_LENGTH = 4;
 const goods = [
     {name: 'T-shirt man', price: 50, urlSingle: 'single_page.html', srcImg: 'img/Item1.jpg', altImg: 'T-shirt man' },
     {name: 'Rouse-blose', price: 52, urlSingle: 'single_page.html', srcImg: 'img/Item2.jpg', altImg: 'Rouse-blose' },
@@ -11,24 +11,23 @@ const goods = [
     {name: 'Blue hoodie', price: 59, urlSingle: 'single_page.html', altImg: 'Blue hoodie' }
 ];
 
-var catalogueGoods = [];
-for (let i = 0; i < CATALOGUE_LENGTH && i < goods.length; i++) {
-    catalogueGoods.push(goods[i]);
+var featuredGoods = [];
+for (let i = 0; i < FEAUTERED_COUNT; i++) { //Берем только определенное количество эл-тов, что-бы не испортить главную страницу.
+    featuredGoods.push(goods[i]);
 }
 
 const renderItem = ({name, price, urlSingle, srcImg = 'img/NoPhoto.jpg', altImg = 'good'}) => `<div class="item"><a href= ${urlSingle}><img src= ${srcImg} alt=${altImg}><div class="item-text"><p>${name}</p><h3>$${price}</h3></div></a><a href="#cart" class="add">Add to Card</a></div>`;
-
-var k = CATALOGUE_ROW_LENGTH - (catalogueGoods.length % CATALOGUE_ROW_LENGTH);
-
+var k = FEAUTERED_ROW_LENGTH - (featuredGoods.length % FEAUTERED_ROW_LENGTH);
 const renderList = items => document.querySelector('.items').innerHTML = items.map(renderItem).join('') + addCearItems('.items', k); // При выводе массива, между элементами расставляются запятые. Чтобы избавиться от них, применил метот join.
- 
-function addCearItems(parent, k) { // Функция добавления пустых элементов для корректного расположения элементов.
-        let clearItemsTags = ''; 
-    for (let i = 0; i < k && k != CATALOGUE_ROW_LENGTH; i++) { 
+
+function addCearItems(parent, k) {
+        let clearItemsTags = '';
+    for (let i = 0; i < k && k != FEAUTERED_ROW_LENGTH; i++) { 
         clearItemsTags += (document.querySelector(parent).innerHTML = `<div class="item-clear"></div>`);
     }
         return clearItemsTags;
 }
-    
-renderList(goods);
+renderList(featuredGoods);
+
+
 
