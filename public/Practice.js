d568d77f5e7a385d5b8d004275930f444f333892
DenDor <denisdorogov@yandex.ru>
2019-04-04 14:56:41 +0500

    Практическое задание привйл в рабочее состояние.

--- public/Practice.js
+++ public/Practice.js
@@ -1,4 +1,4 @@
-// Урок 3
+// Урок 5
 const API_URL = 'http://localhost:3000/catalog.json';
 class GoodsItem {
     constructor(title, price) {
@@ -10,67 +10,90 @@ class GoodsItem {
     }
 }
 
-class GoodsList {
-    constructor() {
-        this.goods = [];
-        this.filteredGoods = [];
-    };
-    //    promise.then(resolve) {
-    fetchGoods() {
-        return makeGETRequest(`${API_URL}`).then((goods) => {
-            this.goods = JSON.parse(goods);
-            this.filteredGoods = JSON.parse(goods);
-        });
-    };
-    render() {
-        let listHtml = '';
-        this.filteredGoods.forEach(good => {
-            const goodItem = new GoodsItem(good.title, good.price);
-            listHtml += goodItem.render();
-        });
-        document.querySelector('.goods-list').innerHTML = listHtml;
-    }
-    filterGoods(value) {
-        const regexp = new RegExp(value, 'i');
-        this.filteredGoods = this.goods.filter(good =>
-            regexp.test(good.product_name));
-        this.render();
-    }
-}
+//class GoodsList {
+//    constructor() {
+//        this.goods = [];
+//        this.filteredGoods = [];
+//    };
+//    //    promise.then(resolve) {
+//    fetchGoods() {
+//        return makeGETRequest(`${API_URL}`).then((goods) => {
+//            this.goods = JSON.parse(goods);
+//            this.filteredGoods = JSON.parse(goods);
+//        });
+//    };
+//    render() {
+//        let listHtml = '';
+//        this.filteredGoods.forEach(good => {
+//            const goodItem = new GoodsItem(good.title, good.price);
+//            listHtml += goodItem.render();
+//        });
+//        document.querySelector('.goods-list').innerHTML = listHtml;
+//    }
+//    filterGoods(value) {
+//        const regexp = new RegExp(value, 'i');
+//        this.filteredGoods = this.goods.filter(good =>
+//            regexp.test(good.product_name));
+//        this.render();
+//    }
+//}
 
 
-const list = new GoodsList(); // Создаём экземпляр класса GoodsList
-list.fetchGoods().then(() => {
-    list.render();
-});
-
-//const promise = new Promise((resolve, reject) => {});
-
-function makeGETRequest(url) {
-    return new Promise((resolve, reject) => {
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', url); // Определение запроса
-        xhr.send(); // Отправка запроса 
-        if (window.XMLHttpRequest) {
-            xhr = new XMLHttpRequest();
-        } else if (window.ActiveXObject) {
-            xhr = new ActiveXObject("Microsoft.XMLHTTP");
+//const list = new GoodsList(); // Создаём экземпляр класса GoodsList
+//list.fetchGoods().then(() => {
+//    list.render();
+//});
+//
+//let searchButton = document.querySelector('.search-button');
+//let searchInput = document.querySelector('.goods-search');
+//
+//searchButton.addEventListener('click', (e) => {
+//    const value = searchInput.value;
+//    list.filterGoods(value);
+//});
+let searchText;
+const app = new Vue({
+    el: '#app',
+    data: {
+        goods: [],
+        filteredGoods: [],
+        searchLine: '',
+    },
+//    methods: {
+//        makeGETRequest(url, callback) {
+//            const API_URL = 'http://localhost:3000/catalog.json';
+//            let xhr;
+//            if (window.XMLHttpRequest) {
+//                xhr = new XMLHttpRequest();
+//            } else if (window.ActiveXObject) {
+//                xhr = new ActiveXObject('Microsoft.XMLHTTP');
+//            };
+//            xhr.onreadystatechange = function () { // Подписка на события
+//                if (xhr.readyState === XMLHttpRequest.DONE) {
+//                    callback(xhr.responseText);
+//                }
+//                xhr.open('GET', url); // Определение запроса
+//                xhr.send(); // Отправка запроса
+//            }
+//        }
+//    },
+//    mounted() {
+//        this.makeGETRequest(`$API_URL`, (goods) => {
+//            this.goods = goods;
+//            this.filteredGoods = goods;
+//        });
+//    }
+    mounted() {
+        fetch('http://localhost:3000/catalog.json')
+        .then(response => response.json())
+        .then((goods) => {
+            this.goods = goods;
+            this.filteredGoods = goods;
+        })
+    },
+    methods: {
+        handleSearchClick() {
+            alert('Find');
         }
-        xhr.onreadystatechange = function () { // Подписка на события
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-                if (xhr.status === 200) {
-                    resolve(JSON.parse(xhr.responseText));
-                } else reject();
-            }
-        }
-
-    });
-
-};
-let searchButton = document.querySelector('.search-button');
-let searchInput = document.querySelector('.goods-search');
-
-searchButton.addEventListener('click', (e) => {
-    const value = searchInput.value;
-    list.filterGoods(value);
+    }
 });
