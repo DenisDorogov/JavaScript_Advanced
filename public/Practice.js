13e23ce7f50688116071483c845441ae6b1ff877
DenDor <denisdorogov@yandex.ru>
2019-04-04 17:53:15 +0500

    Попытался сделать. Поиск работает некорректно, не сбрасывает при чистом поле. filteredGoods не восстанавливается после поиска. Дальше времени и мозгов не хватило.

--- public/Practice.js
+++ public/Practice.js
@@ -9,48 +9,6 @@ class GoodsItem {
         return `<div class = "goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
     }
 }
-
-//class GoodsList {
-//    constructor() {
-//        this.goods = [];
-//        this.filteredGoods = [];
-//    };
-//    //    promise.then(resolve) {
-//    fetchGoods() {
-//        return makeGETRequest(`${API_URL}`).then((goods) => {
-//            this.goods = JSON.parse(goods);
-//            this.filteredGoods = JSON.parse(goods);
-//        });
-//    };
-//    render() {
-//        let listHtml = '';
-//        this.filteredGoods.forEach(good => {
-//            const goodItem = new GoodsItem(good.title, good.price);
-//            listHtml += goodItem.render();
-//        });
-//        document.querySelector('.goods-list').innerHTML = listHtml;
-//    }
-//    filterGoods(value) {
-//        const regexp = new RegExp(value, 'i');
-//        this.filteredGoods = this.goods.filter(good =>
-//            regexp.test(good.product_name));
-//        this.render();
-//    }
-//}
-
-
-//const list = new GoodsList(); // Создаём экземпляр класса GoodsList
-//list.fetchGoods().then(() => {
-//    list.render();
-//});
-//
-//let searchButton = document.querySelector('.search-button');
-//let searchInput = document.querySelector('.goods-search');
-//
-//searchButton.addEventListener('click', (e) => {
-//    const value = searchInput.value;
-//    list.filterGoods(value);
-//});
 let searchText;
 const app = new Vue({
     el: '#app',
@@ -59,41 +17,29 @@ const app = new Vue({
         filteredGoods: [],
         searchLine: '',
     },
-//    methods: {
-//        makeGETRequest(url, callback) {
-//            const API_URL = 'http://localhost:3000/catalog.json';
-//            let xhr;
-//            if (window.XMLHttpRequest) {
-//                xhr = new XMLHttpRequest();
-//            } else if (window.ActiveXObject) {
-//                xhr = new ActiveXObject('Microsoft.XMLHTTP');
-//            };
-//            xhr.onreadystatechange = function () { // Подписка на события
-//                if (xhr.readyState === XMLHttpRequest.DONE) {
-//                    callback(xhr.responseText);
-//                }
-//                xhr.open('GET', url); // Определение запроса
-//                xhr.send(); // Отправка запроса
-//            }
-//        }
-//    },
-//    mounted() {
-//        this.makeGETRequest(`$API_URL`, (goods) => {
-//            this.goods = goods;
-//            this.filteredGoods = goods;
-//        });
-//    }
     mounted() {
         fetch('http://localhost:3000/catalog.json')
-        .then(response => response.json())
-        .then((goods) => {
-            this.goods = goods;
-            this.filteredGoods = goods;
-        })
+            .then(response => response.json())
+            .then((goods) => {
+                this.goods = goods;
+                this.filteredGoods = goods;
+            })
     },
     methods: {
         handleSearchClick() {
-            alert('Find');
+//            this.filteredGoods = this.goods.filter((good) => { // Не успел другим методом сделать.
+//                return good.indexOf(this.searchLine) != -1;
+//            });
+            let filteredGoods = this.goods; 
+            let j = filteredGoods.length;
+            for (i = 0; i < j; i++) { // Работает не совсем корректно
+                console.log('iteration ' + i);
+                if (filteredGoods[i].title.indexOf(this.searchLine) == -1) {
+                    filteredGoods.splice(i,1);
+                    console.log('deleted '+i);
+//                    i--;
+                }
+            }
         }
     }
 });
