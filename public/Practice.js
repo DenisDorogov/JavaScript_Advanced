b5dd43f2b60a1c5c100f909d4f2b5b10df555ebf
DenDor <denisdorogov@yandex.ru>
2019-04-05 13:04:53 +0500

    Реализовано практическое задание с урока №6. Всё работает.

--- public/Practice.js
+++ public/Practice.js
@@ -1,45 +1,101 @@
-// Урок 5
-const API_URL = 'http://localhost:3000/catalog.json';
-class GoodsItem {
-    constructor(title, price) {
-        this.title = title;
-        this.price = price;
-    }
-    render() {
-        return `<div class = "goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
-    }
-}
-let searchText;
+// Урок 6
+const API_URL = 'http://localhost:3000';
 const app = new Vue({
     el: '#app',
     data: {
-        goods: [],
-        filteredGoods: [],
-        searchLine: '',
+        items: [],
+        filteredItems: [],
+        searchQuery: '',
+        cart: [],
     },
     mounted() {
-        fetch('http://localhost:3000/catalog.json')
+        fetch(`${API_URL}/products`) // Запрос продуктов.
             .then(response => response.json())
-            .then((goods) => {
-                this.goods = goods;
-                this.filteredGoods = goods;
-            })
+            .then((items) => {
+                this.items = items;
+                this.filteredItems = items;
+            });
+        fetch(`${API_URL}/cart`) // Запрос корзины.
+            .then(response => response.json())
+            .then((items) => {
+                this.cart = items;
+            });
+    },
+    computed: {
+        total() {
+            return this.cart.reduce((acc, item) => acc + item.price * item.quantity, 0)
+        }
     },
     methods: {
-        handleSearchClick() {
-//            this.filteredGoods = this.goods.filter((good) => { // Не успел другим методом сделать.
-//                return good.indexOf(this.searchLine) != -1;
-//            });
-            let filteredGoods = this.goods; 
-            let j = filteredGoods.length;
-            for (i = 0; i < j; i++) { // Работает не совсем корректно
-                console.log('iteration ' + i);
-                if (filteredGoods[i].title.indexOf(this.searchLine) == -1) {
-                    filteredGoods.splice(i,1);
-                    console.log('deleted '+i);
-//                    i--;
-                }
+        handleDeleteClick(item) { // Удаление по кнопке.
+            if (item.quantity > 1) { // Если... то убавляем количество
+                fetch(`${API_URL}/cart/${item.id}`, {
+                        method: 'PATCH',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            ...item,
+                            quantity: item.quantity - 1
+                        })
+                    })
+                    .then(response => response.json())
+                    .then((item) => {
+                        const itemIdx = this.cart.findIndex((entry) => entry.id === item.id);
+                        //                    this.cart[itemIdx].quantity = item.quantity;
+                        Vue.set(this.cart, itemIdx, item); // Если Vue не отображает предыдущий вариант.
+                    });
+            } else { // Если... то убираем элемент.
+                fetch(`${API_URL}/cart/${item.id}`, {
+                        method: 'DELETE',
+                    })
+                    .then(() => { // Удаляем из корзины элемент с соответствующим id
+                        this.cart = this.cart.filter((cartItem) => cartItem.id !== item.id)
+                    })
             }
+
+        },
+        handleSearchClick() { // Функция поиска
+            const regexp = new RegExp(this.searchQuery, 'i'); // регулярное выражение для поиска, 'i' - параметр для выравнивания регистра.
+            this.filteredItems = this.items.filter((item) => regexp.test(item.name))
+        },
+        handleByeClick(item) { // Функция добавления товара в корзину.
+            const cartItem = this.cart.find((entry) => entry.id === item.id) // Проверяем есть ли товар в корзине.
+            if (cartItem) { // Товар в корзине уже есть.
+                fetch(`${API_URL}/cart/${item.id}`, {
+                        method: 'PATCH',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            ...item,
+                            quantity: cartItem.quantity + 1
+                        })
+                    })
+                    .then(response => response.json())
+                    .then((item) => {
+                        const itemIdx = this.cart.findIndex((entry) => entry.id === item.id);
+                        //                    this.cart[itemIdx].quantity = item.quantity;
+                        Vue.set(this.cart, itemIdx, item);
+                    });
+            } else { // Товара в корзине нет.
+                fetch(`${API_URL}/cart`, {
+                        method: 'POST', // Метод запроса
+                        headers: {
+                            'Content-Type': 'application/json',
+                        }, // Заголовки, которые нам нужны.
+                        body: JSON.stringify({
+                            ...item,
+                            quantity: 1
+                        }) // То что хотим отправить.
+                    })
+                    .then(response => response.json())
+                    .then((item) => {
+                        this.cart.push(item);
+                    });
+            }
+
         }
     }
+
 });
