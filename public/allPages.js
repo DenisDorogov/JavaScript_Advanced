2975fcd48d66e8fb0ab66403162d8ab35c56da1b
DenDor <denisdorogov@yandex.ru>
2019-04-17 19:12:55 +0500

    Начал работу с классом корзины.

--- public/allPages.js
+++ public/allPages.js
@@ -1,14 +1,34 @@
 class Cart {
-    constructor() {}
-    
-    addCartItem() {}
-    
-    removeCartItem() {}
+    constructor() {};
 
-    changeCartItem() {}
-    
-    
-}
+    addCartItem(event) {
+        if (event.target.className === 'add') { // Если нажата кнопка очистки корзины.
+            console.log('Сработал метод класса.');
+            console.log(event);
+            buildCart(cart);
+            getTotalCart(cart);
+            catalogCountsClear();
+        }
+        if (event.target.className === 'cart-item-clear') { // Если нажата кнопка удаления элемента.
+            for (i = 0; i < cart.length; i++) {
+                if (event.target.id == 'cart-item-clear' + i) {
+                    cart[i].count = 1;
+                    cart.splice(i, 1);
+                }
+            }
+            buildCart(cart);
+            getTotalCart(cart);
+        }
+    };
+
+    removeCartItem() {};
+
+    changeCartItem() {};
+};
+
+
+
+const cart = new Cart();
 
 const $header = document.querySelector('.container_header');
 
