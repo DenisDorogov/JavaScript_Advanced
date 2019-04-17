3a492f6c822c602607fe4e408e81bf12df4c916f
DenDor <denisdorogov@yandex.ru>
2019-04-17 11:59:06 +0500

    Начал делать корзину. Перенес файл в public.

---
+++ public/allPages.js
@@ -0,0 +1,20 @@
+class Cart {
+    constructor() {}
+    
+    addCartItem() {}
+    
+    removeCartItem() {}
+
+    changeCartItem() {}
+    
+    
+}
+
+const $header = document.querySelector('.container_header');
+
+//$header.addEventListener('click', (event) => {
+//    switch(event.target.id) {
+//            case 'myAccount'
+//    }
+//    
+//})
